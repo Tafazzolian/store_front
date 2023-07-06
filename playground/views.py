5593077dc5e6b5f3bf6ef978a151942144947629
tafazzolian <tafazzolyan@gmail.com>
2023-07-06 14:06:21 +0330

    my ongoing store project

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
+from django.db.models.aggregates import Count, Sum, Avg, Min, Max
+from django.db.models.functions import Concat
+from store.models import Product, Customer, OrderItem, Order, Collection
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    last_order_id = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField()) 
+    query_set = Product.objects.annotate(
+        money_spent=Sum(F('orderitem__unit_price')*F('orderitem__quantity'))).order_by('-money_spent')[:5]
+
+    return render(request, 'hello.html', {'queryset': query_set})
