29664c3a379c0122f254316f5bc874f24699dffa
tafazzolian <tafazzolyan@gmail.com>
2023-07-16 00:05:44 +0330

    generic models

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
-from django.db.models.aggregates import Count, Sum, Avg, Min, Max
+from django.contrib.contenttypes.models import ContentType
 from django.db.models.functions import Concat
 from store.models import Product, Customer, OrderItem, Order, Collection
+from tags.models import TaggedItem
 
 #query testing
 def say_hello(request):
-    query_set = Customer.objects.annotate(result = Max('order__id'))
+    content_type = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects\
+        .select_related('tag')\
+        .filter(
+        content_type=content_type,
+        object_id = 1,
+        )
 
     return render(request, 'hello.html', {'queryset': query_set})
